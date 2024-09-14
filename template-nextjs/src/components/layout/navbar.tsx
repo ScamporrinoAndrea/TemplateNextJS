692f1fad29607def7d5b74c179c2dd5fdff81ae6
Skank16 <scampo.andrea@gmail.com>
2024-09-14 11:30:02 +0200

    initial commit

---
+++ template-nextjs/src/components/layout/navbar.tsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+        <div className="bg-white">
+            <div className="flex max-w-7xl mx-auto p-8 items-center">
+                <div className="flex-1">
+                    logo
+                </div>
+                <div>
+                    login
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
